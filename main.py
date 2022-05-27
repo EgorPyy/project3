bef4abf4b5252de9f2d8ec32bceb19472557425b
Egor Shpak <pizi.1997@mail.ru>
2022-05-27 16:41:29 +0300

    version2

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 list = []
+my_file = open('d:/data.txt', 'w+', encoding='utf-8')
 while True:
     thing = input ('Что у вас?')
+    my_file.write(thing + '\n')
     if thing:
         list.append(thing)
         print('Спасибо')
@@ -11,3 +13,4 @@ while True:
             print('Вот вам',last_thing)
         else:
             print('Извините,подойдите позже')
+my_file.close()
